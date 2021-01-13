2b9f0f23ef3958a16798c512a0f33e04a7e12b44
ywxie <30338551+Yiwenzxyzhh@users.noreply.github.com>
2021-01-13 14:02:24 +0800

    27. 移除元素

--- array/ArraySolution.py
+++ array/ArraySolution.py
@@ -1,20 +1,27 @@
-class Solution:
-    # 35. 搜索插入位置
-    def search_insert(nums, target):
-        length = len(nums)
-        l = 0
-        r = length - 1
-        while l <= r:
-            mid = l + ((r - l) / 2)
-            if nums.index(mid) > target:
-                r = mid - 1
-            elif nums.index(mid) < target:
-                l = mid + 1
-            else:
-                return mid
-        return r + 1
+# from typing import List
+
+# 35. 搜索插入位置
+from typing import List
 
 
-    # 27. 移除元素
-    def removeElement(self, nums: List[int], val: int) -> int:
-        nums.remove(val)
+def search_insert(nums, target):
+    length = len(nums)
+    l = 0
+    r = length - 1
+    while l <= r:
+        mid = l + ((r - l) / 2)
+        if nums.index(mid) > target:
+            r = mid - 1
+        elif nums.index(mid) < target:
+            l = mid + 1
+        else:
+            return mid
+    return r + 1
+
+
+# 27. 移除元素
+def removeElement(nums: List[int], val: int) -> int:
+        for num in nums:
+            while nums.count(val) > 0:
+                nums.remove(val)
+        return len(nums)
